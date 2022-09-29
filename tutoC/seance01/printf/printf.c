33f15721b6b9aa146745d4b4c4ba8e20315af53e
Filipe Vasconcelos <filipe.vasconcelos@esme.fr>
2022-09-29 15:23:48 +0200

    seance01

---
+++ tutoC/seance01/printf/printf.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+// Module    : Parcours Innovation 
+// promo     : INGE1
+// date      : 29/09/22
+// Formateur : F. Vasconcelos
+// Séance 3  : Valeur d'un char sortie formaté %c et %d 
+******************************************************************************/
+#include<stdio.h>
+int main()
+{
+    char c;
+    c='z';
+    printf("valeur décimal de z : %d\n",c);
+    printf("valeur chr     de z : %c\n",c);
+    // boucle sur les char de a à z
+    for(char c='a';c<'z';c++)
+    {
+        printf("%d %c\n",c,c);
+    }
+    return 0;
+}
