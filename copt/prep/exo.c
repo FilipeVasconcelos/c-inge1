f61b012cb323407b5003fdf23c23b2c426ad0439
Filipe Vasconcelos <filipe.manuel.vasconcelos@gmail.com>
2023-10-17 10:21:57 +0200

    before

---
+++ copt/prep/exo.c
@@ -0,0 +1,20 @@
+/* Au programme :
+ *  Tableaux et pointeurs.
+ *  Tableaux et chaînes de caractères
+*/
+
+
+/*
+ * Quelques Exercices pratiques : 
+ 
+  Inverser une chaîne de caractères saisie par l'utilisateur.
+ 
+  Écrivez une fonction qui prend un pointeur sur un tableau d'entiers 
+  et qui incrémente tous les éléments du tableau de 1.
+
+  Écrivez une fonction qui prend un pointeur sur une structure 
+  et qui modifie la valeur d'un des membres de la structure.
+
+  Écrivez un programme qui alloue dynamiquement un tableau d'entiers 
+  et qui affiche le contenu du tableau.
+*/
